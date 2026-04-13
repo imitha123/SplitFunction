5dba0eb94f45cfb6ed7bfda7b8de1a2f79afde50
Imitha Maneli <ST10483606@rcconnect.edu.za>
2026-04-13 12:06:15 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SplitFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] keywords = { "software", "web" , "array", "oop", "debug"};
+
+            string[] response = { "Software Development is the process of designingm building, testing and maintaining software applcation",
+                                  "Web Development focusrs on building websites and web appplication",
+                                  "An array is a data structure that stores dfferent data types such as string,int,double etc",
+                                  "OOP is object oriented programming that has four pillars, Encapsulation, Polymorphyism, Abstraction and inheritance",
+                                  "Debugging is the process of identifying and fixing errors"
+
+            };
+           
+        }
+    }
+}
